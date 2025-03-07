6953b8806716bc80976d821fa8275943e54277fb
diegoestrada11 <est21016@byui.edu>
2025-03-06 20:25:47 -0700

    added angle.cpp and angle.h

--- 230.09.Ponder/angle.cpp
+++ 230.09.Ponder/angle.cpp
@@ -2,7 +2,7 @@
  * Source File:
  *    ANGLE
  * Author:
- *    <your name here>
+ *    Br. Helfrich
  * Summary:
  *    Everything we need to know about a direction
  ************************************************************************/
@@ -12,13 +12,134 @@
 #include <cassert>
 using namespace std;
 
- /************************************
-  * ANGLE : NORMALIZE
-  ************************************/
+/******************************
+ * ANGLE : Default constructor
+ ******************************/
+Angle::Angle() : radians(0.0) {}
+
+/***************************
+ * ANGLE : Copy constructor
+ ***************************/
+Angle::Angle(const Angle& rhs) : radians(rhs.radians) {}
+
+/**********************************
+ * ANGLE : Constructor with degrees
+ **********************************/
+Angle::Angle(double degrees) : radians(convertToRadians(degrees)) {}
+
+/*************************************
+ * ANGLE : Converts radians to degrees
+ *************************************/
+double Angle::convertToDegrees() const
+{
+   return radians * (180.0 / M_PI);  // Convert radians to degrees
+}
+
+/*************************************
+ * ANGLE : Converts degrees to radians
+ *************************************/
+double Angle::convertToRadians(double degrees) const
+{
+   return degrees * (M_PI / 180.0);  // Convert degrees to radians
+}
+
+/*************************************************
+ * ANGLE : Normalize radians
+ * Prevents radians from going over or under 2 Pi
+ *************************************************/
 double Angle::normalize(double radians) const
 {
-   return 9.9;
+   const double TWO_PI = 2 * M_PI;
+
+   // Normalize the angle to be within [0, 2 * M_PI]
+   radians = fmod(radians, TWO_PI);
+
+   // If radians is negative, make it positive by adding TWO_PI
+   if (radians < 0)
+      radians += TWO_PI;
+
+   return radians;
 }
 
 
+/***********************************
+ * ANGLE : Get the angle in radians
+ ***********************************/
+double Angle::getRadians() const
+{
+   return radians;
+}
 
+/**********************************
+ * ANGLE : Get the angle in degrees
+ **********************************/
+double Angle::getDegrees() const
+{
+   return convertToDegrees();
+}
+
+/*************************************
+ * ANGLE : Set the angle using degrees
+ *************************************/
+void Angle::setDegrees(double degrees)
+{
+   radians = convertToRadians(degrees);
+   radians = normalize(radians);
+}
+
+/*************************************
+ * ANGLE : Set the angle using radians
+ *************************************/
+void Angle::setRadians(double radians)
+{
+   this->radians = normalize(radians);
+}
+
+/*****************************************
+ * ANGLE : Set the angle to 0 radians (up)
+ *****************************************/
+void Angle::setUp()
+{
+   radians = 0.0;
+}
+
+/********************************************
+ * ANGLE : Set the angle to Pi radians (down)
+ ********************************************/
+void Angle::setDown()
+{
+   radians = M_PI;
+}
+
+/***********************************************
+ * ANGLE : Set the angle to Pi/2 radians (right)
+ ***********************************************/
+void Angle::setRight()
+{
+   radians = M_PI / 2;
+}
+
+/***********************************************
+ * ANGLE : Set the angle to 3Pi/2 radians (left)
+ ***********************************************/
+void Angle::setLeft()
+{
+   radians = 3 * M_PI / 2;
+}
+
+/**********************************************
+ * ANGLE : Reverse the direction by Pi radians
+ **********************************************/
+void Angle::reverse()
+{
+   radians = normalize(radians + M_PI);
+}
+
+/********************************************
+ * ANGLE : Add a delta to the current radians
+ ********************************************/
+Angle& Angle::add(double delta)
+{
+   radians = normalize(radians + delta);
+   return *this;
+}
--- 230.09.Ponder/angle.h
+++ 230.09.Ponder/angle.h
@@ -2,7 +2,7 @@
  * Header File:
  *    ANGLE
  * Author:
- *    <your name here>
+ *    Br. Helfrich
  * Summary:
  *    Everything we need to know about a direction
  ************************************************************************/
@@ -12,89 +12,45 @@
 #define _USE_MATH_DEFINES
 #include <math.h>   // for M_PI which is 3.14159
 
- // for the unit tests
-class TestAngle;
 class TestPosition;
 class TestVelocity;
 class TestAcceleration;
-class TestHowitzer;
-class TestProjectile;
+class TestAngle;
+class TestLander;
 
- /************************************
-  * ANGLE
-  ************************************/
+/************************************
+ * ANGLE
+ ************************************/
 class Angle
 {
 public:
-   // for the unit tests
    friend TestAcceleration;
    friend TestVelocity;
    friend TestAngle;
-   friend TestHowitzer;
-   friend TestProjectile;
+   friend TestLander;
 
    // Constructors
-   Angle()                  : radians(9.9)         {}
-   Angle(const Angle& rhs)  : radians(9.9)         {}
-   Angle(double degrees)    : radians(9.9)         {}
+   Angle();                                        // Default constructor
+   Angle(const Angle& rhs);                        // Copy constructor
+   Angle(double degrees);                          // Constructor with degrees
 
    // Getters
-   double getDegrees() const { return 9.9; }
-   double getRadians() const { return 9.9; }
-
-   //         dx
-   //    +-------/
-   //    |      /
-   // dy |     /
-   //    |    / 1.0
-   //    | a /
-   //    |  /
-   //    | /
-   // dy = cos a
-   // dx = sin a
-   double getDx() const { return 9.9; }
-   double getDy() const { return 9.9; }
-   bool   isRight()          const { return true; }
-   bool   isLeft()           const { return true; }
-
+   double getDegrees() const;                      // Get the angle in degrees
+   double getRadians() const;                      // Get the angle in radians
 
    // Setters
-   void setDegrees(double degrees) { }
-   void setRadians(double radians) { }
-   void setUp()                    { }
-   void setDown()                  { }
-   void setRight()                 { }
-   void setLeft()                  { }
-   void reverse()                  { }
-   Angle& add(double delta)        { return *this; }
-
-   // set based on the components
-   //         dx
-   //     +-------/
-   //     |      /
-   //  dy |     /
-   //     |    /
-   //     | a /
-   //     |  /
-   //     | /
-   void setDxDy(double dx, double dy)  { }
-   Angle operator+(double degrees) const { return Angle(); }
+   void setDegrees(double degrees);                // Set using degrees
+   void setRadians(double radians);                // Set using radians
+   void setUp();                                   // Set to 0 radians (up)
+   void setDown();                                 // Set to Pi radians (down)
+   void setRight();                                // Set to Pi/2 radians (right)
+   void setLeft();                                 // Set to 3Pi/2 radians (left)
+   void reverse();                                 // Reverse the direction by Pi radians
+   Angle& add(double delta);                       // Add a delta in radians
 
 private:
-
-   double normalize(double radians) const;
-
-   double radians;   // 360 degrees equals 2 PI radians
+   double normalize(double radians) const;         // Normalize radians between (0, 2Pi)
+   double convertToDegrees() const;                // Convert radians to degrees
+   double convertToRadians(double degrees) const;  // Convert degrees to radians
+   double radians;                                 // 360 degrees equals 2 PI radians
 };
-
-#include <iostream>
-
-/*******************************************************
- * OUTPUT ANGLE
- * place output on the screen in degrees
- *******************************************************/
-inline std::ostream& operator << (std::ostream& out, const Angle& rhs)
-{
-   out << rhs.getDegrees() << "degree";
-   return out;
-}
