82bcf70bc47349764306a3c744a42cf161fcf8bd
Noah <120699382+NmFlash8@users.noreply.github.com>
2025-03-13 19:41:29 -0600

    Physics

--- 230.10.Ponder/physics.cpp
+++ 230.10.Ponder/physics.cpp
@@ -6,27 +6,8 @@
  * Summary:
  *    Laws of motion, effects of gravity, wind resistence, etc.
  ************************************************************************/
-  
- #include "physics.h"  // for the prototypes
- 
 
- /*********************************************************
- * calculateFromTable
- * From a list of domains and ranges, linear interpolate
- *********************************************************/
- // Table-based function for calculating gravity, speed of sound, density, and drag
-/*double calculateFromTable(double altitude, const vector<pair<double, double>>& table)
-{
-   for (size_t i = 0; i < table.size() - 1; ++i)
-   {
-      if (altitude >= table[i].first && altitude <= table[i + 1].first)
-      {
-         return linearInterpolation(altitude, table[i].first, table[i + 1].first,
-            table[i].second, table[i + 1].second);
-      }
-   }
-   return -1;  // Return -1 if no valid range is found
-}
+#include "physics.h"  // for the prototypes
 
 
  /*********************************************************
@@ -70,8 +51,17 @@ double linearInterpolation(const Mapping mapping[], int numMapping, double domai
  *********************************************************/
 double gravityFromAltitude(double altitude)
 {
-   return -99.9;
+   const Mapping gravityTable[] = {
+       {0, 9.807},    {1000, 9.804},  {2000, 9.801},  {3000, 9.797},  {4000, 9.794},
+       {5000, 9.791}, {6000, 9.788},  {7000, 9.785},  {8000, 9.782},  {9000, 9.779},
+       {10000, 9.776}, {15000, 9.761}, {20000, 9.745}, {25000, 9.730}, {30000, 9.715},
+       {40000, 9.684}, {50000, 9.654}, {60000, 9.624}, {70000, 9.594}, {80000, 9.564}
+   };
+
+   int numMapping = sizeof(gravityTable) / sizeof(gravityTable[0]);
+   return linearInterpolation(gravityTable, numMapping, altitude);
 }
+
 
 /*********************************************************
  * DENSITY FROM ALTITUDE
@@ -79,7 +69,14 @@ double gravityFromAltitude(double altitude)
  *********************************************************/
 double densityFromAltitude(double altitude)
 {
-   return -99.9;
+   const Mapping densityTable[] = {
+       {0, 1.225},         {1000, 1.112},       {2000, 1.007},       {3000, 0.9093},      {4000, 0.8194},
+       {5000, 0.7364},     {6000, 0.6601},      {7000, 0.59},        {8000, 0.5258},      {9000, 0.4671},
+       {10000, 0.4135},    {15000, 0.1948},     {20000, 0.08891},    {25000, 0.04008},    {30000, 0.01841},
+       {40000, 0.003996},  {50000, 0.001027},   {60000, 0.0003097},  {70000, 0.0000828},  {80000, 0.0000185}
+   };
+   int numMapping = sizeof(densityTable) / sizeof(densityTable[0]);
+   return linearInterpolation(densityTable, numMapping, altitude);
 }
 
 /*********************************************************
@@ -88,7 +85,14 @@ double densityFromAltitude(double altitude)
  ********************************************************/
 double speedSoundFromAltitude(double altitude)
 {
-   return -99.9;
+   const Mapping speedOfSoundTable[] = {
+       {0, 340},     {1000, 336},   {2000, 332},   {3000, 328},   {4000, 324},   {5000, 320},
+       {6000, 316},  {7000, 312},   {8000, 308},   {9000, 303},   {10000, 299},  {15000, 295},
+       {20000, 295}, {25000, 295},  {30000, 305},  {40000, 324},  {50000, 337},  {60000, 319},
+       {70000, 289}, {80000, 269}
+   };
+   int numMapping = sizeof(speedOfSoundTable) / sizeof(speedOfSoundTable[0]);
+   return linearInterpolation(speedOfSoundTable, numMapping, altitude);
 }
 
 
@@ -98,6 +102,13 @@ double speedSoundFromAltitude(double altitude)
  *********************************************************/
 double dragFromMach(double speedMach)
 {
-   return -99.9;
+   const Mapping dragTable[] = {
+       {0.300, 0.1629}, {0.500, 0.1659}, {0.700, 0.2031}, {0.890, 0.2597}, {0.920, 0.3010},
+       {0.960, 0.3287}, {0.980, 0.4002}, {1.000, 0.4258}, {1.020, 0.4335}, {1.060, 0.4483},
+       {1.240, 0.4064}, {1.530, 0.3663}, {1.990, 0.2897}, {2.870, 0.2297}, {2.890, 0.2306},
+       {5.000, 0.2656}
+   };
+   int numMapping = sizeof(dragTable) / sizeof(dragTable[0]);
+   return linearInterpolation(dragTable, numMapping, speedMach);
 }
 
