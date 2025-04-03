b9d7e4cb53c79f7e9cee03176d4e5c1e3c7fedb9
Noah <120699382+NmFlash8@users.noreply.github.com>
2025-04-02 21:53:27 -0600

    howitzer 83%

--- 230.12.Ponder/angle.h
+++ 230.12.Ponder/angle.h
@@ -27,6 +27,7 @@ public:
    friend TestVelocity;
    friend TestAngle;
 
+
    // Constructors
    Angle();                                        // Default constructor
    Angle(const Angle& rhs);                        // Copy constructor
@@ -41,15 +42,15 @@ public:
    bool   isLeft()     const;                      // Check if angle is left side
 
    // Setters
-   void setDegrees(double degrees);                // Set using degrees
-   void setRadians(double radians);                // Set using radians
-   void setUp();                                   // Set to 0 radians (up)
-   void setDown();                                 // Set to Pi radians (down)
-   void setRight();                                // Set to Pi/2 radians (right)
-   void setLeft();                                 // Set to 3Pi/2 radians (left)
-   void reverse();                                 // Reverse the direction by Pi radians
+   void   setDegrees(double degrees);              // Set using degrees
+   void   setRadians(double radians);              // Set using radians
+   void   setUp();                                 // Set to 0 radians (up)
+   void   setDown();                               // Set to Pi radians (down)
+   void   setRight();                              // Set to Pi/2 radians (right)
+   void   setLeft();                               // Set to 3Pi/2 radians (left)
+   void   reverse();                               // Reverse the direction by Pi radians
    Angle& add(double delta);                       // Add a delta in radians
-   void setDxDy(double dx, double dy);             // Set angle based on components
+   void   setDxDy(double dx, double dy);           // Set angle based on components
 
 private:
    double normalize(double radians) const;         // Normalize radians between (0, 2Pi)
